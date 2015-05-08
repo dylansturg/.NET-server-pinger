4ddff69f1c84c3b0c71a63036cfc98552207f5ef
Dylan Sturgeon <dylansturg@gmail.com>
2015-05-08 15:00:41 -0400

    Base project and ServerPinger class to represent a worker that repeatedly requests the same Uri.  Optionally waits a given TimeSpan between requests.  Optionally logs to a RequestLogger.

---
+++ ServerPinger/ServerPinger/RequestLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPinger
+{
+    public class RequestLogger
+    {
+
+
+        public async void LogSuccessfulRequest(Uri requestedUri)
+        {
+            // TODO fill this in
+        }
+
+        public async void LogFailureRequest(Uri requestedUri)
+        {
+            // TODO fill this in
+        }
+    }
+}
---
+++ ServerPinger/ServerPinger/ServerPinger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerPinger
+{
+    public class ServerPinger
+    {
+        public Uri RequestUri { get; set; }
+        public TimeSpan TimeDelay { get; set; }
+        public RequestLogger Log { get; set; }
+
+        public ServerPinger()
+        {   
+        }
+
+        public ServerPinger(Uri request, TimeSpan delay, RequestLogger log)
+        {
+            RequestUri = request;
+            TimeDelay = delay;
+            Log = log;
+        }
+
+        public async void BeginPinging(CancellationToken cancelToken)
+        {
+            if (RequestUri == null)
+            {
+                throw new ArgumentNullException("RequestUri");
+            }
+
+            while (!cancelToken.IsCancellationRequested)
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(RequestUri, cancelToken);
+                    if (Log == null) continue;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Log.LogSuccessfulRequest(RequestUri);
+                    }
+                    else
+                    {
+                        Log.LogSuccessfulRequest(RequestUri);
+                    }
+                }
+
+                if (TimeDelay > TimeSpan.Zero)
+                {
+                    Thread.Sleep(TimeDelay);
+                }
+            }
+        }
+
+    }
+}
